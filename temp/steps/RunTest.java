67b51e9a9f84143859cb54af76d6458372ec2721
NGavrilova <gavrilova_179@mail.ru>
2020-09-06 21:50:07 +0400

    add DataProvider

--- temp/steps/RunTest.java
+++ temp/steps/RunTest.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources"},
-        glue = {"steps"}, plugin = {"pretty"})
+        glue = {"temp/steps"}, plugin = {"pretty"})
 public class RunTest {
 }
